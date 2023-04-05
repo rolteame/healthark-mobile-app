c1a422a0dc1700b85e1db6675a8290e4957bc020
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2023-04-05 18:20:06 +0100

    create forgot password page and reset link page, fix some bugs

---
+++ src/views/ForgotPasswordPage.vue
@@ -0,0 +1,107 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <section id="forgot-password-section">
+        <div class="forgot-password-wrapper">
+          <h2>Forget password?</h2>
+          <p>
+            Enter the email address associated with this account and your
+            password reset link will be sent to you
+          </p>
+          <form action="#" class="forgot-password-form">
+            <ion-label>Email</ion-label>
+            <ion-input placeholder="Enter Email"></ion-input>
+            <ion-button> Reset password </ion-button>
+          </form>
+          <p>Or login with</p>
+          <!-- Social Links starts here -->
+          <SocialLinks />
+        </div>
+      </section>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonInput, IonLabel, IonButton, IonContent } from "@ionic/vue";
+import SocialLinks from "../components/SocialLinks.vue";
+export default {
+  components: {
+    // ionic components
+    IonPage,
+    IonInput,
+    IonLabel,
+    IonButton,
+    IonContent,
+    // social links component
+    SocialLinks,
+  },
+  setup() {},
+};
+</script>
+
+<style scoped>
+* {
+  font-family: var(--ff-satoshi) !important;
+  /* font-weight: var(--fw-700) !important; */
+}
+ion-content {
+  --padding-top: 20px;
+  --padding-bottom: 20px;
+  --padding-start: 20px;
+  --padding-end: 20px;
+  --background: var(--bg-gradient);
+  overflow-y: hidden !important;
+}
+#forgot-password-section {
+  min-height: 92vh;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.forgot-password-wrapper {
+  width: 100%;
+}
+.forgot-password-wrapper > h2 {
+  text-align: center;
+  font-size: var(--fs-28);
+  color: #fff;
+  font-weight: var(--fw-700);
+  margin-bottom: 20px;
+}
+.forgot-password-wrapper > p {
+  font-weight: var(--fw-400);
+  font-size: var(--fs-14);
+  line-height: 19px;
+  text-align: center;
+  color: #d6d6d6;
+}
+ion-label {
+  font-size: var(--fs-14);
+  margin-top: 15px !important;
+  display: block;
+  color: #fff !important;
+}
+ion-input {
+  --background: #d6d6d6;
+  border-radius: 8px;
+  color: #697386;
+  margin-top: 10px;
+}
+ion-button {
+  border-radius: 4px;
+  width: 100%;
+  margin-top: 18px;
+  --background: #0f5b8f !important;
+  text-transform: none !important;
+  font-size: 16px;
+  font-weight: var(--fw-700);
+}
+.forgot-password-form + p {
+  margin-top: 16px;
+  font-weight: var(--fw-700);
+  font-size: var(--fs-14);
+  color: #d5d5d5;
+}
+</style>
---
+++ src/views/ResetLinkPage.vue
@@ -0,0 +1,83 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <section id="reset-link-section">
+        <div class="reset-link-wrapper">
+          <p class="reset-link-text">Reset link sent to</p>
+          <p class="reset-email">obendalee@gmail.com</p>
+          <form action="#">
+            <ion-button> Continue to login </ion-button>
+          </form>
+        </div>
+      </section>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonButton, IonContent } from "@ionic/vue";
+export default {
+  components: {
+    // ionic components
+    IonPage,
+    IonButton,
+    IonContent,
+  },
+  setup() {},
+};
+</script>
+
+<style scoped>
+* {
+  font-family: var(--ff-satoshi) !important;
+  /* font-weight: var(--fw-700) !important; */
+}
+ion-content {
+  --padding-top: 20px;
+  --padding-bottom: 20px;
+  --padding-start: 20px;
+  --padding-end: 20px;
+  --background: var(--bg-gradient);
+  overflow-y: hidden !important;
+}
+#reset-link-section {
+  min-height: 92vh;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.reset-link-wrapper {
+  width: 100%;
+}
+.reset-link-wrapper > .reset-link-text {
+  font-weight: var(--fw-400);
+  font-size: var(--fs-16);
+  line-height: 22px;
+  text-align: center;
+  color: #d6d6d6;
+}
+.reset-link-wrapper > p {
+  font-weight: var(--fw-400);
+  font-size: var(--fs-14);
+  line-height: 19px;
+  text-align: center;
+  color: #d6d6d6;
+}
+.reset-link-wrapper > .reset-email {
+  font-weight: var(--fw-700);
+  font-size: var(--fs-16);
+  line-height: 22px;
+  text-align: center;
+}
+ion-button {
+  border-radius: 4px;
+  width: 100%;
+  margin-top: 18px;
+  --background: #0f5b8f !important;
+  text-transform: none !important;
+  font-size: 16px;
+  font-weight: var(--fw-700);
+  color: #d6d6d6;
+}
+</style>
